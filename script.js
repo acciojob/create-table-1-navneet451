73c00f82b57526421b1b45bbbca14e607bf4f241
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-10-20 13:01:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function insert_Row() {
 	let newRow = table.insertRow(0);
     let cell1 = newRow.insertCell(0);
     let cell2 = newRow.insertCell(1);
-	cell1.innerHTML = "New cell1";
-    cell2.innerHTML = "New cell2";
+	cell1.innerHTML = "New Cell1";
+    cell2.innerHTML = "New Cell2";
 
   
 }
